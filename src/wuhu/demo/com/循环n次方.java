a283db5d805c0fee1986f43a8b9eea9bc0345dad
zhangjian <zhangjian@cc.com>
2022-09-19 13:19:00 +0800

    frist commit

---
+++ src/wuhu/demo/com/循环n次方.java
@@ -0,0 +1,20 @@
+package wuhu.demo.com;
+
+import java.util.Scanner;
+
+public class 循环n次方 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x=0;
+        for(int i=1;i<=n;i++) {
+            for (int j = 1; j <= i; j++)
+                for (int k = 1; k <= j; k++) {
+                    x = x + 1;
+                    System.out.print(x + ",");
+                }
+           System.out.println('\n');
+        }
+
+    }
+}
